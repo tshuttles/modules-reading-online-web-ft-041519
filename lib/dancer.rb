3518321899a9035c4e91c085b034c6a4485487a1
Tachi Shuttlesworth <tshuttlesworth@gmail.com>
2019-05-09 04:31:03 +0000

    Done.

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,2 +1,20 @@
 require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
+require_relative './fancy_dance.rb'
+
+class Dancer  
+  
+  include Dance
+  extend MetaDancing
+  attr_accessor :name
+ 
+  def initialize(name)
+    @name = name
+  end
+  
+  class Dancer
+  extend FancyDance::ClassMethods
+  include FancyDance::InstanceMethods
+  end
+  
+end 
