5f36153c9a3e158011f5360bba25ab7cba6976b0
Tachi Shuttlesworth <tshuttlesworth@gmail.com>
2019-05-09 04:20:16 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -2,12 +2,8 @@ require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
 
 class Kid 
-  include Dance 
-  extend MetaDancing
-  
-  attr_accessor :name 
-  
-  def initialize(name)
-    @name = name 
-  end 
+  class Dancer
+  extend FancyDance::ClassMethods
+  include FancyDance::InstanceMethods
+  end
 end 
